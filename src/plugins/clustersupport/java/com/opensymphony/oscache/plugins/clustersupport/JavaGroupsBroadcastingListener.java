703472c1cd80f47457a2db053a0384eec30cc09f
dres <dres@76ce2dab-00d6-b540-f576-6e23f3d3abcb>
2004-11-29 01:08:08 +0000

    Submitter: Andres March Date: November 28th, 2004
    Updated javagroups jar and configuration based on
    recommendations from maintainer (Bela Ban).  Besides
    updating to v. 2.2.7, I changed some timeout settings and
    moved STABLE protocol above UNICAST.

--- src/plugins/clustersupport/java/com/opensymphony/oscache/plugins/clustersupport/JavaGroupsBroadcastingListener.java
+++ src/plugins/clustersupport/java/com/opensymphony/oscache/plugins/clustersupport/JavaGroupsBroadcastingListener.java
@@ -44,9 +44,17 @@ public class JavaGroupsBroadcastingListener extends AbstractBroadcastingListener
     /**
     * The first half of the default channel properties. They default channel properties are:
     * <pre>
-    * UDP(mcast_addr=*.*.*.*;mcast_port=45566;ip_ttl=32;mcast_send_buf_size=150000;mcast_recv_buf_size=80000):PING(timeout=2000;num_initial_members=3):MERGE2(min_interval=5000;max_interval=10000):FD_SOCK:VERIFY_SUSPECT(timeout=1500):pbcast.NAKACK(gc_lag=50;retransmit_timeout=300,600,1200,2400,4800):pbcast.STABLE(desired_avg_gossip=20000):UNICAST(timeout=5000):FRAG(frag_size=8096;down_thread=false;up_thread=false):pbcast.GMS(join_timeout=5000;join_retry_timeout=2000;shun=false;print_local_addr=true)
+    * UDP(mcast_addr=*.*.*.*;mcast_port=45566;ip_ttl=32;\
+    * mcast_send_buf_size=150000;mcast_recv_buf_size=80000):\
+    * PING(timeout=2000;num_initial_members=3):\
+    * MERGE2(min_interval=5000;max_interval=10000):\
+    * FD_SOCK:VERIFY_SUSPECT(timeout=1500):\
+    * pbcast.NAKACK(gc_lag=50;retransmit_timeout=300,600,1200,2400,4800;max_xmit_size=8192):\
+    * UNICAST(timeout=300,600,1200,2400):\
+    * pbcast.STABLE(desired_avg_gossip=20000):\
+    * FRAG(frag_size=8096;down_thread=false;up_thread=false):\
+    * pbcast.GMS(join_timeout=5000;join_retry_timeout=2000;shun=false;print_local_addr=true)
     * </pre>
-    *
     *
     * Where <code>*.*.*.*</code> is the specified multicast IP, which defaults to <code>231.12.21.132</code>.
     */
@@ -55,11 +63,21 @@ public class JavaGroupsBroadcastingListener extends AbstractBroadcastingListener
     /**
     * The second half of the default channel properties. They default channel properties are:
     * <pre>
-    * UDP(mcast_addr=*.*.*.*;mcast_port=45566;ip_ttl=32;mcast_send_buf_size=150000;mcast_recv_buf_size=80000):PING(timeout=2000;num_initial_members=3):MERGE2(min_interval=5000;max_interval=10000):FD_SOCK:VERIFY_SUSPECT(timeout=1500):pbcast.NAKACK(gc_lag=50;retransmit_timeout=300,600,1200,2400,4800):pbcast.STABLE(desired_avg_gossip=20000):UNICAST(timeout=5000):FRAG(frag_size=8096;down_thread=false;up_thread=false):pbcast.GMS(join_timeout=5000;join_retry_timeout=2000;shun=false;print_local_addr=true)
+    * UDP(mcast_addr=*.*.*.*;mcast_port=45566;ip_ttl=32;\
+    * mcast_send_buf_size=150000;mcast_recv_buf_size=80000):\
+    * PING(timeout=2000;num_initial_members=3):\
+    * MERGE2(min_interval=5000;max_interval=10000):\
+    * FD_SOCK:VERIFY_SUSPECT(timeout=1500):\
+    * pbcast.NAKACK(gc_lag=50;retransmit_timeout=300,600,1200,2400,4800;max_xmit_size=8192):\
+    * UNICAST(timeout=300,600,1200,2400):\
+    * pbcast.STABLE(desired_avg_gossip=20000):\
+    * FRAG(frag_size=8096;down_thread=false;up_thread=false):\
+    * pbcast.GMS(join_timeout=5000;join_retry_timeout=2000;shun=false;print_local_addr=true)
     * </pre>
+    *
     * Where <code>*.*.*.*</code> is the specified multicast IP, which defaults to <code>231.12.21.132</code>.
     */
-    private static final String DEFAULT_CHANNEL_PROPERTIES_POST = ";mcast_port=45566;ip_ttl=32;mcast_send_buf_size=150000;mcast_recv_buf_size=80000):PING(timeout=2000;num_initial_members=3):MERGE2(min_interval=5000;max_interval=10000):FD_SOCK:VERIFY_SUSPECT(timeout=1500):pbcast.NAKACK(gc_lag=50;retransmit_timeout=300,600,1200,2400,4800):pbcast.STABLE(desired_avg_gossip=20000):UNICAST(timeout=5000):FRAG(frag_size=8096;down_thread=false;up_thread=false):pbcast.GMS(join_timeout=5000;join_retry_timeout=2000;shun=false;print_local_addr=true)";
+    private static final String DEFAULT_CHANNEL_PROPERTIES_POST = ";mcast_port=45566;ip_ttl=32;mcast_send_buf_size=150000;mcast_recv_buf_size=80000):" + "PING(timeout=2000;num_initial_members=3):MERGE2(min_interval=5000;max_interval=10000):FD_SOCK:VERIFY_SUSPECT(timeout=1500):" + "pbcast.NAKACK(gc_lag=50;retransmit_timeout=300,600,1200,2400,4800;max_xmit_size=8192):UNICAST(timeout=300,600,1200,2400):pbcast.STABLE(desired_avg_gossip=20000):" + "FRAG(frag_size=8096;down_thread=false;up_thread=false):pbcast.GMS(join_timeout=5000;join_retry_timeout=2000;shun=false;print_local_addr=true)";
     private static final String DEFAULT_MULTICAST_IP = "231.12.21.132";
     private NotificationBus bus;
 
