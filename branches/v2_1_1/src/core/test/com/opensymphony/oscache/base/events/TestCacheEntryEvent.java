a0642b70546ee89f7a55f89c7ee8f97ebf5e8792
nobody <nobody@76ce2dab-00d6-b540-f576-6e23f3d3abcb>
2005-02-27 13:54:47 +0000

    This commit was manufactured by cvs2svn to create branch 'v2_1_1'.

---
+++ branches/v2_1_1/src/core/test/com/opensymphony/oscache/base/events/TestCacheEntryEvent.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2002-2003 by OpenSymphony
+ * All rights reserved.
+ */
+package com.opensymphony.oscache.base.events;
+
+import com.opensymphony.oscache.base.Cache;
+import com.opensymphony.oscache.base.CacheEntry;
+import com.opensymphony.oscache.general.GeneralCacheAdministrator;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * This is the test class for the CacheEntryEvent class. It checks that the
+ * public methods are working properly
+ *
+ * $Id$
+ * @version        $Revision$
+ * @author <a href="mailto:abergevin@pyxis-tech.com">Alain Bergevin</a>
+ */
+public final class TestCacheEntryEvent extends TestCase {
+    /**
+     * Constants required for the test
+     */
+    private final String KEY = "Test cache entry event key";
+    private final int SCOPE = 3;
+
+    /**
+     * Constructor
+     * <p>
+     * @param str The test name (required by JUnit)
+     */
+    public TestCacheEntryEvent(String str) {
+        super(str);
+    }
+
+    /**
+     * This methods returns the name of this test class to JUnit
+     * <p>
+     * @return The test for this class
+     */
+    public static Test suite() {
+        return new TestSuite(TestCacheEntryEvent.class);
+    }
+
+    /**
+     * Test the CacheEntryEvent class
+     */
+    public void testCacheEntryEvent() {
+        // Create all the required objects
+        GeneralCacheAdministrator admin = new GeneralCacheAdministrator();
+        Cache map = new Cache(admin.isMemoryCaching(), admin.isUnlimitedDiskCache(), admin.isOverflowPersistence());
+        CacheEntry entry = new CacheEntry(KEY);
+        CacheEntryEvent event = new CacheEntryEvent(map, entry, null);
+
+        // Get back the values and assert them
+        assertEquals(event.getEntry(), entry);
+        assertEquals(event.getKey(), KEY);
+        assertEquals(event.getMap(), map);
+    }
+}
