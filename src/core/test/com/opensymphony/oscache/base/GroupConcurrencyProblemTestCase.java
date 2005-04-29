5e4ad9b7df427d9d9c157bc5c1cda1accaa2cc76
dres <dres@76ce2dab-00d6-b540-f576-6e23f3d3abcb>
2005-04-29 07:23:47 +0000

    Issue number:  CACHE-170 Submitted by:  Guillaume Berche Reviewed by:   Andres March
    Had to change the management of the update states to ensure 2 states are not created for the same key, thereby keeping threads from being notified properly. One thing to revisit is the placement of the startUpdate call.  Realistically this should be at the beginning of the put() but I think this will perform better and should not cause problems.

--- src/core/test/com/opensymphony/oscache/base/GroupConcurrencyProblemTestCase.java
+++ src/core/test/com/opensymphony/oscache/base/GroupConcurrencyProblemTestCase.java
@@ -1,55 +1,59 @@
+/*
+ * Copyright (c) 2002-2003 by OpenSymphony
+ * All rights reserved.
+ */
 package com.opensymphony.oscache.base;
-
-import junit.framework.TestCase;
 
 import com.opensymphony.oscache.general.GeneralCacheAdministrator;
 
+import junit.framework.TestCase;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author $author$
+ * @version $Revision$
+ */
 public class GroupConcurrencyProblemTestCase extends TestCase {
-	private static GeneralCacheAdministrator cache = new GeneralCacheAdministrator();
+    private static GeneralCacheAdministrator cache = new GeneralCacheAdministrator();
 
-	public static void main( String[] args ) {
-		System.out.println( "START" );
+    public static void main(String[] args) {
+        System.out.println("START");
 
-		// Create some clients and start them running.
-		for ( int i = 0; i < 100; i++ ) {
-			System.out.println( "Creating thread: " + i );
+        // Create some clients and start them running.
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Creating thread: " + i);
 
-			new Client( i, cache ).start();
-		}
+            new Client(i, cache).start();
+        }
 
-		System.out.println( "END" );
-	}
+        System.out.println("END");
+    }
 }
+
 
 /* Inner class to hammer away at the cache. */
 class Client extends Thread {
-	private static final int MAX_ITERATIONS = 1000;
+    private static final int MAX_ITERATIONS = 1000;
+    private GeneralCacheAdministrator cache;
+    private int id;
 
-	private int id;
-	private GeneralCacheAdministrator cache;
+    public Client(int newId, GeneralCacheAdministrator newCache) {
+        super();
+        id = newId;
+        cache = newCache;
+    }
 
-	public Client(
-		int newId,
-		GeneralCacheAdministrator newCache
-	) {
-		super();
-		id = newId;
-		cache = newCache;
-	}
+    public void run() {
+        for (int i = 0; i < MAX_ITERATIONS; i++) {
+            /* Put an entry from this Client into the shared group.
+             */
+            cache.putInCache(Integer.toString(id), "Some interesting data", new String[] {
+                    "GLOBAL_GROUP"
+                });
 
-	public void run() {
-		for ( int i = 0; i < MAX_ITERATIONS; i++ ) {
-
-			/* Put an entry from this Client into the shared group.
-			 */
-			cache.putInCache(
-				Integer.toString( id ),
-				"Some interesting data",
-				new String[] { "GLOBAL_GROUP" }
-			);
-
-			// Flush that group.
-			cache.flushGroup( "GLOBAL_GROUP" );
-		}
-	}
+            // Flush that group.
+            cache.flushGroup("GLOBAL_GROUP");
+        }
+    }
 }
