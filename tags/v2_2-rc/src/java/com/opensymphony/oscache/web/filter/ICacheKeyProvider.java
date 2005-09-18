d99a6fb57830ccb4817ec0927c0fd7433d7a4880
nobody <nobody@76ce2dab-00d6-b540-f576-6e23f3d3abcb>
2005-09-18 19:59:59 +0000

    This commit was manufactured by cvs2svn to create tag 'v2_2-rc'.

---
+++ tags/v2_2-rc/src/java/com/opensymphony/oscache/web/filter/ICacheKeyProvider.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2002-2003 by OpenSymphony
+ * All rights reserved.
+ */
+package com.opensymphony.oscache.web.filter;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.opensymphony.oscache.base.Cache;
+import com.opensymphony.oscache.web.ServletCacheAdministrator;
+
+/**
+ * Provider interface for cache key creation. A developer can implement a method which provides
+ * cache keys based on the request, the servlect cache administrator and cache.
+ * 
+ * JIRA issue: http://jira.opensymphony.com/browse/CACHE-179
+ *
+ * @author <a href="mailto:ltorunski@t-online.de">Lars Torunski</a>
+ * 
+ * @version $Revision$
+ */
+public interface ICacheKeyProvider {
+
+    /**
+     * Creates the cache key for the CacheFilter.
+     *
+     * @param httpRequest the http request.
+     * @param scAdmin the ServletCacheAdministrator of the cache
+     * @param cache the cache of the ServletCacheAdministrator
+     * @return the cache key
+     */
+    public String createCacheKey(HttpServletRequest httpRequest, ServletCacheAdministrator scAdmin, Cache cache);
+
+}
